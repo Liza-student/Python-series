d96b0ab3fd4777f20c8199642821562ac9609a1f
Lizarani Behera <lizaranib718@gmail.com>
2025-06-30 18:59:36 +0530

    Add files via upload

---
+++ coding challenge.py
@@ -0,0 +1,9 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print(
+    "Result:"
+)
+print("Addition: ", a + b)
+print("Subtraction: ", a - b)
+print("multiplication: ", a * b)
+print("division: ", a / b)
